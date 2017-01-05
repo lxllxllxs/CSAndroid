59660bc4d4262570f537f84dfb4576887c156c77
lxllxllxs <77478516@qq.com>
2017-01-05 22:56:00 +0800

    还是要用Fragment

--- app/src/main/java/com/yiyekeji/coolschool/ui/MainViewpagerActivity.java
+++ app/src/main/java/com/yiyekeji/coolschool/ui/MainViewpagerActivity.java
@@ -1,18 +1,10 @@
 package com.yiyekeji.coolschool.ui;
 
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 
 import com.yiyekeji.coolschool.R;
 import com.yiyekeji.coolschool.ui.base.BaseActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainViewpagerActivity extends BaseActivity {
 
@@ -24,46 +16,10 @@ public class MainViewpagerActivity extends BaseActivity {
 		initView();
 		initViewPager();
 	}
-
 	private void initViewPager() {
 
 	}
-
 	private void initView() {
-
-	}
-	
-	private class MyPagerAdapter extends PagerAdapter {
-
-		List<View> list = new ArrayList<View>();
-
-		private MyPagerAdapter(ArrayList<View> list) {
-			this.list = list;
-		}
-
-		public int getCount() {
-			return list.size();
-		}
-
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return arg0 == arg1;
-		}
-
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			ViewPager pViewPager = ((ViewPager) container);
-			pViewPager.removeView(list.get(position));
-		}
-
-		public Object instantiateItem(View arg0, int arg1) {
-			ViewPager pViewPager = ((ViewPager) arg0);
-			pViewPager.addView(list.get(arg1));
-			return list.get(arg1);
-		}
-
-		public Parcelable saveState() {
-			return null;
-		}
-
 	}
 
 }
