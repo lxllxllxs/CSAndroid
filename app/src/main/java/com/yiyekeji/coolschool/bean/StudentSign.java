90d7f889134377d75db7faf8ece8006a71189415
lxllxllxs <lxl774785161>
2017-01-09 17:33:14 +0800

    主界面

---
+++ app/src/main/java/com/yiyekeji/coolschool/bean/StudentSign.java
@@ -0,0 +1,22 @@
+package com.yiyekeji.coolschool.bean;
+
+/**
+ * Created by lxl on 2017/1/9.
+ */
+public class StudentSign {
+    private String userNo;
+
+    private String realName;
+    private String isSignIn;
+    private String IMEI;
+    private String IP;
+
+
+
+
+/*    private float longitude;//经度
+    private float latitude;//纬度*/
+
+
+    private String courseNo;//课程编号
+}
